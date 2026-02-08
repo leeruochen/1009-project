4642bc8f74293ada89b96ee079bd009e51ad4228
Xavier <xaviergoh0502@gmail.com>
2026-02-09 00:36:52 +0800

    Update Scene.java

--- libgdx/lwjgl3/src/main/java/github/com_1009project/lwjgl3/Scene.java
+++ libgdx/lwjgl3/src/main/java/github/com_1009project/lwjgl3/Scene.java
@@ -3,17 +3,18 @@ package github.com_1009project.lwjgl3;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.badlogic.gdx.Gdx;
+
 import github.com_1009project.abstractEngine.ResourceManager;
 
 public class Scene {
     private int id;
-    private List<Layer> layers;
+    private List<Layer> layers = new ArrayList<>();;
     private ResourceManager resourceManager;
 
     public Scene(int id, ResourceManager resourceManager) {
         this.id = id;
         this.resourceManager = resourceManager;
-        this.layers = new ArrayList<>();
     }
     public int getId() {
         return id;
@@ -22,9 +23,18 @@ public class Scene {
         // add BackgroundLayer and EntityLayer
         layers.add(new BackgroundLayer(resourceManager));
         layers.add(new EntityLayer(resourceManager));
+        layers.add(new UILayer());
     }
     public void onEnter() {
         // called when scene becomes active
+        for (Layer layer : layers) {
+            if (layer instanceof UILayer) {
+                Gdx.input.setInputProcessor(((UILayer) layer).getUIManager().getStage());
+            }
+        }
+    }
+    public void onExit() {
+        // used to stop music or other things when scene is no longer active
     }
     public void update(float deltaTime) {
         for (Layer layer : layers) {
