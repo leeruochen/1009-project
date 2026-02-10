37f009fe3c4d9aa7a0bd1d611a60de3ea30ab803
fanight7109 <tan_jonathan@yahoo.com.sg>
2026-02-11 00:09:40 +0800

    Update EntityFactory.java

--- libgdx/core/src/main/java/github/com_1009project/abstractEngine/EntityFactory.java
+++ libgdx/core/src/main/java/github/com_1009project/abstractEngine/EntityFactory.java
@@ -1,16 +1,16 @@
 // package github.com_1009project.abstractEngine;
 
-public class EntityFactory {
-    public Entity createEntity(EntityType type) {
-        switch (type) {
-            case TRIANGLE:
-                return new Triangle();
-            case SQUARE:
-                return new Square();
-            case CIRCLE:
-                return new Circle();
-            default:
-                return null;
-        }
-    }
-}
+// public class EntityFactory {
+//     public Entity createEntity(EntityType type) {
+//         switch (type) {
+//             case TRIANGLE:
+//                 return new Triangle();
+//             case SQUARE:
+//                 return new Square();
+//             case CIRCLE:
+//                 return new Circle();
+//             default:
+//                 return null;
+//         }
+//     }
+// }
