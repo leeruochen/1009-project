0cb06db4815cf07a83259c295f2f61259ccde459
leeruochen <leeruochen7@gmail.com>
2026-02-14 17:35:34 +0800

    dd

--- libgdx/core/src/main/java/github/com_1009project/abstractEngine/SceneManager.java
+++ libgdx/core/src/main/java/github/com_1009project/abstractEngine/SceneManager.java
@@ -1,44 +1,44 @@
-// package github.com_1009project.abstractEngine;
+package github.com_1009project.abstractEngine;
 
-// import java.util.HashMap;
-// import java.util.Map;
+import java.util.HashMap;
+import java.util.Map;
 
-// public class SceneManager {
-//     private Map<Integer, Scene> scenes = new HashMap<>();
-//     private Scene currentScene;
-//     private ResourceManager resourceManager; // Reference to ResourceManager
+public class SceneManager {
+    private Map<Integer, Scene> scenes = new HashMap<>();
+    private Scene currentScene;
+    private ResourceManager resourceManager; // Reference to ResourceManager
 
-//     public SceneManager(ResourceManager resourceManager) {
-//         this.resourceManager = resourceManager;
-//     }
-//     public void loadScene(int id) {
-//         Scene scene = new Scene(id, resourceManager);
-//         scene.init();
-//         scenes.put(id, scene);
-//     }
-//     public void switchScene(int id) {
-//         if (currentScene != null) {
-//             currentScene.dispose();
-//         }
-//         currentScene = scenes.get(id);
-//         if (currentScene != null) {
-//             currentScene.init();
-//         }
-//     }
-//     public void updateScene(float deltaTime) {
-//         if (currentScene != null) {
-//             currentScene.update(deltaTime);
-//         }
-//     }
-//     public void renderScene() {
-//         if (currentScene != null) {
-//             currentScene.render();
-//         }
-//     }
-//     public void dispose() {
-//         for (Scene scene : scenes.values()) {
-//             scene.dispose();
-//         }
-//         scenes.clear();
-//     }
-// }
+    public SceneManager(ResourceManager resourceManager) {
+        this.resourceManager = resourceManager;
+    }
+    public void loadScene(int id) {
+        Scene scene = new Scene(id, resourceManager);
+        scene.init();
+        scenes.put(id, scene);
+    }
+    public void switchScene(int id) {
+        if (currentScene != null) {
+            currentScene.dispose();
+        }
+        currentScene = scenes.get(id);
+        if (currentScene != null) {
+            currentScene.init();
+        }
+    }
+    public void updateScene(float deltaTime) {
+        if (currentScene != null) {
+            currentScene.update(deltaTime);
+        }
+    }
+    public void renderScene() {
+        if (currentScene != null) {
+            currentScene.render();
+        }
+    }
+    public void dispose() {
+        for (Scene scene : scenes.values()) {
+            scene.dispose();
+        }
+        scenes.clear();
+    }
+}
