caf930e0f44d2d4accd5f4ff49c01c9632eb2646
xchin02 <83194359+xchin02@users.noreply.github.com>
2026-02-16 20:30:16 +0800

    Update MovementManager.java - movementManager is now an event observer

--- libgdx/core/src/main/java/github/com_1009project/abstractEngine/MovementManager.java
+++ libgdx/core/src/main/java/github/com_1009project/abstractEngine/MovementManager.java
@@ -1,195 +1,113 @@
-package github.com_1009project;
+package github.com_1009project.abstractEngine;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 
-import github.com_1009project.abstractEngine.CameraManager;
-import github.com_1009project.abstractEngine.CollisionManager;
-import github.com_1009project.abstractEngine.MapManager;
-import github.com_1009project.abstractEngine.EntityManager;
-import github.com_1009project.abstractEngine.Entity;
-import github.com_1009project.abstractEngine.testEntity;
-import github.com_1009project.abstractEngine.EventManager;
-import github.com_1009project.abstractEngine.MovementManager;
-import github.com_1009project.abstractEngine.SceneManager;
-import github.com_1009project.abstractEngine.UIFactory;
-import github.com_1009project.abstractEngine.Event;
-import com.badlogic.gdx.Input;
-
-public class GameMaster extends ApplicationAdapter{
+public class MovementManager implements EventObserver {
+    private final float playerSpeed = 300f;
     private EntityManager entityManager;
-    private SceneManager sm;
-    private EventManager eventManager;
-    private MovementManager movementManager;
-    private UIFactory uf;
-    private CollisionManager collisionManager;
-    private AssetManager assetManager;
-    private CameraManager camera;
-    private MapManager mapManager;
-    private SpriteBatch batch;
-    private testEntity player;
-
-    // camera properties
-    private int width, height;
-
-    public GameMaster(int width, int height) {
-        this.width = width;
-        this.height = height;
+    
+    // Track which keys are currently pressed
+    private boolean leftPressed = false;
+    private boolean rightPressed = false;
+    private boolean upPressed = false;
+    private boolean downPressed = false;
+    private boolean spacePressed = false; //jump logic is in game logic
+    
+    public MovementManager(EntityManager entityManager) {
+    	this.entityManager = entityManager;
     }
 
-    // this is our set up, to initialize our managers and variables
-    @Override
-    public void create() {
-        collisionManager = new CollisionManager(128);
-        batch = new SpriteBatch();
-        assetManager = new AssetManager();
-        entityManager = new EntityManager(assetManager);
-        eventManager = new EventManager();
-        movementManager = new MovementManager(entityManager);
-        sm = new SceneManager(assetManager, entityManager, eventManager, batch);
+    public void handlePlayerInput(Entity entity, Event event, boolean isUp) {
+        if (entity == null) return;
 
-        // set up camera with max world bounds
-        camera = new CameraManager(width, height);
-        camera.setBounds(4000, 4000);
-
-        // load assets
-        // update the asset manager to actually load the assets
-        // finishLoading() makes sure all assets are loaded before proceeding
-        loadAssets();
-        assetManager.update();
-        assetManager.finishLoading();
-
-        // set up the map
-        // scale the map if needed, if textures look small
-        // load the map from file
-        // parse collision layer and add collision boxes to entities list, "Collision" can be changed to how the developer wants to name it in Tiled
-        mapManager = new MapManager(entityManager);
-        mapManager.setScale(4.0f); 
-        mapManager.setMap(assetManager.get("maps/tests.tmx", TiledMap.class));
-        mapManager.loadEntities();
-
-        // example of creating an entity and making it the target of the camera
-        // this makes the camera follow the player entity
-        for (Entity entity : entityManager.getEntities()) {
-            if (entity instanceof testEntity) {
-                player = (testEntity) entity;
-                break;
+        // Update input state
+        if (!isUp) { // Key Pressed
+            switch (event) {
+                case PlayerLeft: 
+                	leftPressed = true; 
+                	break;
+                case PlayerRight: 
+                	rightPressed = true; 
+                	break;
+                case PlayerUp: 
+                	upPressed = true; 
+                	break;
+                case PlayerDown: 
+                	downPressed = true; 
+                	break;
+                case PlayerJump: 
+                	spacePressed = true;
+                	break;
+            }
+        } else { // Key Released
+            switch (event) {
+                case PlayerLeft: 
+                	leftPressed = false; 
+                	break;
+                case PlayerRight: 
+                	rightPressed = false; 
+                	break;
+                case PlayerUp: 
+                	upPressed = false; 
+                	break;
+                case PlayerDown: 
+                	downPressed = false; 
+                	break;
+                case PlayerJump: 
+                	spacePressed = false;
+                	break;
             }
         }
-        camera.setTarget(player);
 
-        //eventmanager adds movementManager as an event observer
-		eventManager.addObserver(movementManager);
-
-		//key mappings for eventManager
-		eventManager.mapKey(Input.Keys.W, Event.PlayerUp);
-		eventManager.mapKey(Input.Keys.S, Event.PlayerDown);
-		eventManager.mapKey(Input.Keys.A, Event.PlayerLeft);
-		eventManager.mapKey(Input.Keys.D, Event.PlayerRight);
-		eventManager.mapKey(Input.Keys.RIGHT, Event.PlayerRight);
-		eventManager.mapKey(Input.Keys.LEFT, Event.PlayerLeft);
-		eventManager.mapKey(Input.Keys.SPACE, Event.PlayerJump);
-        eventManager.mapKey(Input.Keys.E, Event.PlayerInteract);
-
-		// Register input processor
-		Gdx.input.setInputProcessor(eventManager);
+        // Update velocity based on current input state
+        updatePlayerVelocity(entity);
     }
 
-    // our main gameplay/simulation loop
-    // the loop should be process input -> update -> collision -> render
-    @Override
-    public void render() {
-        ScreenUtils.clear(0, 0, 0, 1);
-
-        // time between each frame, this ensures same speed on different devices
-        float deltaTime = Gdx.graphics.getDeltaTime();
-
-        if (player != null && player.mapToLoad != null) {
-            String newMap = player.mapToLoad;
-            player.mapToLoad = null; // reset the mapToLoad variable
-            loadMap(newMap);
-            return;
-        }
-
-        // update all entities
-        entityManager.update(deltaTime);
-
-        // update collisions
-        collisionManager.updateCollision(entityManager.getEntities());
-
-        // update camera position
-        camera.cameraUpdate(deltaTime);
-
-        // render entities and map based on camera position
-        mapManager.render(camera.getCamera());
+    private void updatePlayerVelocity(Entity entity) {
+        Vector2 vel = entity.getMovementComponent().getVelocity();
         
-        if (player.hasCollided) {
-            camera.shake(2f, 0.2f);
-            player.hasCollided = false;
+        // Horizontal movement
+        if (leftPressed && !rightPressed) {
+            vel.x = -playerSpeed;
+        } else if (rightPressed && !leftPressed) {
+            vel.x = playerSpeed;
+        } else {
+            vel.x = 0; // Both or neither pressed
         }
-
-        // batch will render entities according to cameraPosition
-        batch.setProjectionMatrix(camera.getCamera().combined);
-        batch.begin();
-        entityManager.render(batch);
-        batch.end();
-    }
-
-    // this method is used to load a new map, it clears the current entities and loads the new map's entities
-    // used when transitioning between maps, the player entity will have a variable that specifies which map to load, and when the variable is not null, this method will be called with the new map name
-    private void loadMap(String mapName) {
-        entityManager.clear();
-
-        if (!assetManager.isLoaded(mapName)) {
-            assetManager.load(mapName, TiledMap.class);
-            assetManager.finishLoading();
+        
+        // Vertical movement
+        if (upPressed && !downPressed) {
+            vel.y = playerSpeed;
+        } else if (downPressed && !upPressed) {
+            vel.y = -playerSpeed;
+        } else {
+            vel.y = 0; // Both or neither pressed
         }
-        mapManager.setMap(assetManager.get(mapName, TiledMap.class));
-        mapManager.loadEntities();
-
-        for (Entity entity : entityManager.getEntities()) {
-            if (entity instanceof testEntity) {
-                player = (testEntity) entity;
-                break;
-            }
-        }
-        camera.setTarget(player);
+        
     }
-
-    // an example of how to use the asset manager to load assets, this can be expanded to load more assets as needed
-    // can also use a json file or other data file to specify assets to load, and parse that file in this method to load assets in bulk
-    private void loadAssets() {
-        // load textures
-        assetManager.load("imgs/boy_down_1.png", Texture.class);
-
-        // load tmx maps, params required to prevent errors
-        assetManager.setLoader(TiledMap.class, new TmxMapLoader());
-        TmxMapLoader.Parameters params = new TmxMapLoader.Parameters();
-        params.projectFilePath = "maps/test.tiled-project";
-        assetManager.load("imgs/background.png", Texture.class);
-        assetManager.load("maps/test.tmx", TiledMap.class, params);
-        assetManager.load("maps/tests.tmx", TiledMap.class, params);
+    
+    // Call this when player dies, pauses, or something that doesnt involve user input
+    public void resetInput() {
+        leftPressed = false;
+        rightPressed = false;
+        upPressed = false;
+        downPressed = false;
     }
+    
+	@Override
+	public void onNotify(Event event, Boolean up) {
+		// Only loop through entities that have explicitly flagged they want input
+		for (Entity entity : entityManager.getEntities()) {
+			if (entity.isActive()) {
+				if (entity.isInputEnabled()) {
+					handlePlayerInput(entity, event, up);
+				}
+			}
+		}
+	}
 
-    // resize is called whenever ApplicationAdapter detects a change in screen size, this can be used to adjust the camera viewport and other properties as needed
-    @Override
-    public void resize(int width, int height) {
-        camera.resize(width, height);
-        mapManager.resize(width, height);
-    }
+	@Override // this is for handling mouse events
+	public void onNotify(Event event, Boolean up, int screenX, int screenY) {
 
-    @Override
-    public void dispose() {
-        assetManager.dispose();
-        batch.dispose();
-        mapManager.dispose();
-        collisionManager.dispose();
-        sm.dispose();
-    }
+	}
 }
