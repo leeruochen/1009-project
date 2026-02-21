a58b8bf7d30f7a0592373f1726a54c7e256c9676
fanight7109 <tan_jonathan@yahoo.com.sg>
2026-02-21 20:31:11 +0800

    Update GameMaster.java

--- libgdx/core/src/main/java/github/com_1009project/GameMaster.java
+++ libgdx/core/src/main/java/github/com_1009project/GameMaster.java
@@ -18,12 +18,11 @@ import github.com_1009project.abstractEngine.EventManager;
 import github.com_1009project.abstractEngine.MovementManager;
 import github.com_1009project.abstractEngine.SceneManager;
 import github.com_1009project.abstractEngine.UIFactory;
-import github.com_1009project.logicEngine.PauseScene;
-import github.com_1009project.logicEngine.testEntity;
 import github.com_1009project.abstractEngine.Event;
 import com.badlogic.gdx.Input;
 
 public class GameMaster extends ApplicationAdapter{
+    // all attributes and managers should be declared here, and initialized in the create() method, this is where we will manage all our game state and logic, and the main game loop will be in the render() method
     private EntityManager entityManager;
     private SceneManager sm;
     private EventManager eventManager;
@@ -34,7 +33,6 @@ public class GameMaster extends ApplicationAdapter{
     private CameraManager camera;
     private MapManager mapManager;
     private SpriteBatch batch;
-    private testEntity player;
 
     // camera properties
     private int width, height;
@@ -47,6 +45,7 @@ public class GameMaster extends ApplicationAdapter{
     // this is our set up, to initialize our managers and variables
     @Override
     public void create() {
+        // initialize all managers and variables here
         collisionManager = new CollisionManager(128);
         batch = new SpriteBatch();
         assetManager = new AssetManager();
@@ -55,130 +54,90 @@ public class GameMaster extends ApplicationAdapter{
         movementManager = new MovementManager(entityManager);
         sm = new SceneManager(assetManager, entityManager, eventManager, batch);
 
-        // set up camera with max world bounds
-        camera = new CameraManager(width, height);
-        camera.setBounds(4000, 4000);
+        // camera initialization and set up
+        // camera = new CameraManager(width, height);
+        // camera.setBounds(4000, 4000);
 
-        // load assets
+        // an example of how to set up the asset manager
         // update the asset manager to actually load the assets
         // finishLoading() makes sure all assets are loaded before proceeding
-        loadAssets();
-        assetManager.update();
-        assetManager.finishLoading();
+        // loadAssets();
+        // assetManager.update();
+        // assetManager.finishLoading();
 
-        // set up the map
+        // map manager set up
         // scale the map if needed, if textures look small
         // load the map from file
         // parse collision layer and add collision boxes to entities list, "Collision" can be changed to how the developer wants to name it in Tiled
-        mapManager = new MapManager(entityManager);
-        mapManager.setScale(4.0f); 
-        loadMap("maps/test.tmx");
-        
-        sm = new SceneManager(assetManager, entityManager, eventManager, batch);
+        // mapManager = new MapManager(entityManager);
+        // mapManager.setScale(4.0f); 
+        // mapManager.render(camera.getCamera());
+        // mapManager.loadEntities();
 
-        // example of creating an entity and making it the target of the camera
-        // this makes the camera follow the player entity
-        for (Entity entity : entityManager.getEntities()) {
-            if (entity instanceof testEntity) {
-                player = (testEntity) entity;
-                break;
-            }
-        }
-        camera.setTarget(player);
-        sm.loadScene(1);
+        // camera target set up, this can be optional if developer wants static camera.
+        // an example of how to set up a player entity and set the camera to follow it
+        // for (Entity entity : entityManager.getEntities()) {
+        //     if (entity instanceof testEntity) {
+        //         player = (testEntity) entity;
+        //         break;
+        //     }
+        // }
+        // camera.setTarget(player);
 
-        //eventmanager adds movementManager as an event observer
-		eventManager.addObserver(movementManager);
-        eventManager.addObserver(sm); // add SceneManager as an observer to handle pause events
+        // Event Manager initialization and set up
+        // we also add the observers here
+		// eventManager.addObserver(movementManager);
+        // eventManager.addObserver(sm);
 
-		//key mappings for eventManager
-		eventManager.mapKey(Input.Keys.W, Event.PlayerUp);
-		eventManager.mapKey(Input.Keys.S, Event.PlayerDown);
-		eventManager.mapKey(Input.Keys.A, Event.PlayerLeft);
-		eventManager.mapKey(Input.Keys.D, Event.PlayerRight);
-		eventManager.mapKey(Input.Keys.RIGHT, Event.PlayerRight);
-		eventManager.mapKey(Input.Keys.LEFT, Event.PlayerLeft);
-		eventManager.mapKey(Input.Keys.SPACE, Event.PlayerJump);
-        eventManager.mapKey(Input.Keys.E, Event.PlayerInteract);
-        eventManager.mapKey(Input.Keys.ESCAPE, Event.GamePause);
+		// we can set key mappings here
+		// eventManager.mapKey(Input.Keys.W, Event.PlayerUp);
+		// eventManager.mapKey(Input.Keys.S, Event.PlayerDown);
+		// eventManager.mapKey(Input.Keys.A, Event.PlayerLeft);
+		// eventManager.mapKey(Input.Keys.D, Event.PlayerRight);
+		// eventManager.mapKey(Input.Keys.RIGHT, Event.PlayerRight);
+		// eventManager.mapKey(Input.Keys.LEFT, Event.PlayerLeft);
+		// eventManager.mapKey(Input.Keys.SPACE, Event.PlayerJump);
+        // eventManager.mapKey(Input.Keys.E, Event.PlayerInteract);
+        // eventManager.mapKey(Input.Keys.ESCAPE, Event.GamePause);
 
 		// Register input processor
-		Gdx.input.setInputProcessor(eventManager);
+        // this is required for the event manager to receive input events, the event manager will then notify its observers (like movementManager and SceneManager) of the events
+		// Gdx.input.setInputProcessor(eventManager);
     }
 
-    // our main gameplay/simulation loop
-    // the loop should be process input -> update -> collision -> render
+    // the main gameplay/simulation loop should be in the render method, this is called continuously by libGDX, and is where we will update our game state and render everything to the screen
+    // the loop should be process input -> update all managers/entities -> collision -> render
     @Override
     public void render() {
         ScreenUtils.clear(0, 0, 0, 1);
 
         // time between each frame, this ensures same speed on different devices
-        float deltaTime = Gdx.graphics.getDeltaTime();
-
-        // Check if paused
-        if (sm.getCurrentScene() instanceof PauseScene) {
-            // Only update and render the Pause UI
-            sm.updateScene(deltaTime);
-            sm.renderScene();
-            return; // Skip the rest of the game logic below
-        }
-
-        if (player != null && player.mapToLoad != null) {
-            String newMap = player.mapToLoad;
-            player.mapToLoad = null; // reset the mapToLoad variable
-            loadMap(newMap);
-            return;
-        }
+        // float deltaTime = Gdx.graphics.getDeltaTime();
 
         // update all entities
-        entityManager.update(deltaTime);
+        // entityManager.update(deltaTime);
 
         // update collisions
-        collisionManager.updateCollision(entityManager.getEntities());
+        // collisionManager.updateCollision(entityManager.getEntities());
 
         // update camera position
-        camera.cameraUpdate(deltaTime);
+        // camera.cameraUpdate(deltaTime);
 
         // render entities and map based on camera position
-        mapManager.render(camera.getCamera());
-        
-        if (player.hasCollided) {
-            camera.shake(2f, 0.2f);
-            player.hasCollided = false;
-        }
+        // mapManager.render(camera.getCamera());
 
         // batch will render entities according to cameraPosition
-        batch.setProjectionMatrix(camera.getCamera().combined);
-        batch.begin();
-        entityManager.render(batch);
-        batch.end();
-    }
-
-    // this method is used to load a new map, it clears the current entities and loads the new map's entities
-    // used when transitioning between maps, the player entity will have a variable that specifies which map to load, and when the variable is not null, this method will be called with the new map name
-    private void loadMap(String mapName) {
-        entityManager.dispose();
-
-        mapManager.setMap(assetManager.get(mapName, TiledMap.class));
-        System.out.println("Loaded map: " + mapName);
-        mapManager.loadEntities();
-
-        camera.setTarget(player);
+        // batch.setProjectionMatrix(camera.getCamera().combined);
+        // batch.begin();
+        // entityManager.render(batch);
+        // batch.end();
     }
 
     // an example of how to use the asset manager to load assets, this can be expanded to load more assets as needed
     // can also use a json file or other data file to specify assets to load, and parse that file in this method to load assets in bulk
     private void loadAssets() {
         // load textures
-        assetManager.load("imgs/boy_down_1.png", Texture.class);
-
-        // load tmx maps, params required to prevent errors
-        assetManager.setLoader(TiledMap.class, new TmxMapLoader());
-        TmxMapLoader.Parameters params = new TmxMapLoader.Parameters();
-        params.projectFilePath = "maps/test.tiled-project";
-        assetManager.load("imgs/background.png", Texture.class);
-        assetManager.load("maps/test.tmx", TiledMap.class, params);
-        assetManager.load("maps/tests.tmx", TiledMap.class, params);
+        // assetManager.load("example.jpg", Texture.class);
     }
 
     // resize is called whenever ApplicationAdapter detects a change in screen size, this can be used to adjust the camera viewport and other properties as needed
